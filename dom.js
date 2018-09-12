7c381f34070a82aa83d5fa1daa13eeba5382637d
Hye Soo Park <flyjwayur@gmail.com>
2018-09-12 17:00:22 +0300

    Refactor code and make copy functionality (Move static HTML elements to HTML files, separate creating divs and adding colours to divs and remove unnecessary array which uses to store the hexa colours)

--- dom.js
+++ dom.js
@@ -1,71 +1,152 @@
-//17. DOM 
-
-//Create Input div, generate button, Input for number of colors
-//Create HTML elements
-const colorWrapper = document.createElement('div');
-const colorInputDiv = document.createElement('div');
-const inputBtnDiv = document.createElement('div');
-const title = document.createElement('h1');
-const colorInput = document.createElement('input');
-const generatorButton = document.createElement('button');
-const stopButton = document.createElement('button');
-const colorBoard = document.createElement("div");
-
-//Give styles
-colorWrapper.setAttribute('class','colorWrapper');
-colorInputDiv.setAttribute('class','colorInputDiv');
-title.setAttribute('class', 'title');
-inputBtnDiv.setAttribute('class','inputBtnDiv');
-colorInput.setAttribute('class','colorInput');
-generatorButton.setAttribute('class','generatorBtn');
-stopButton.setAttribute('class','stopButton');
-title.textContent = "Hexadecimal Color Generator";
-generatorButton.textContent = "Generate";
-stopButton.textContent = "Stop";
-colorBoard.setAttribute('class','colorBoard');
-colorBoard.style.padding = "3rem";
-
-//Add Elements
-document.body.appendChild(colorWrapper);
-colorWrapper.appendChild(colorInputDiv);
-colorInputDiv.appendChild(title);
-colorInputDiv.appendChild(inputBtnDiv);
-inputBtnDiv.appendChild(colorInput);
-inputBtnDiv.appendChild(generatorButton);
-inputBtnDiv.appendChild(stopButton);
-colorWrapper.appendChild(colorBoard);
-console.log("colorBoard",colorBoard);
+//17. DOM
 
 // a. -i
+/*
 const hexaGenerator = ()=>{
-    //let hexaArr = [];
     let hexaArr = new Array(6);
 
-    /*
+
     for(let i = 0; i < 8; i++){
         hexaArr.push(Math.floor(Math.random() * 10));
-    }*/
+    }
         
-   /*
+
     for(let i =0; i < 8; i++){
         hexaArr[i] = Math.floor(Math.random() * 10);
-    }*/
-
+    }
+    
     let hexaArrWithValue = [...hexaArr].map( 
-        el => el = (Math.floor(Math.random() * 16)).toString(16))
+        el => el = (Math.floor(Math.random() * 16)).toString(16));
     
     let hexaWithsharp = '#'+ hexaArrWithValue.join("");
-    //console.log(hexaWithsharp);
+
     return hexaWithsharp;
+};
+*/
+const hexaGenerator = () => {
+    hexa = "";
+    hexa = hexa + Math.floor(Math.random() * 16777215).toString(16);
+    if(hexa.length = 6){
+        return "#" + hexa;
+    }
+};
+
+const createColorDivs = () => {
+  let size = document.querySelector(".colorInput").value;
+  colorBoard.innerHTML = "";
+
+  if (size >= 5) {
+    for (let i = 0; i < size; i++) {
+      let div = document.createElement("div");
+      div.classList.add("colorDiv");
+      colorBoard.appendChild(div);
+    }
+  } else {
+    alert("Please type more than 5");
+  }
+  document.querySelector(".colorInput").value = "";
+};
+
+const exampleFiveColors = ()=>{
+    for (let i = 0; i <5; i++) {
+        let div = document.createElement("div");
+        div.classList.add("colorDiv");
+        colorBoard.appendChild(div);
+    }
 }
 
-console.log(document.querySelector('.colorInput').value);
+const copyHexaToClipBoard = str => {
+    const area = document.createElement('textarea');
+    area.value = str;
+    console.log("hello", area.value);
+    document.body.appendChild(area);
+    area.select();
+    document.execCommand('copy');
+    document.body.removeChild(area); 
+}
+
+let hexaColor = [];
+
+const addColorsHexaCodeToDiv = () => {
+  let colordivs = document.querySelectorAll(".colorDiv");
+  
+  colordivs.forEach((eachDiv, index) => {
+    hexaColor[index] = hexaGenerator();
+    //console.log("hexaColor[index]",hexaColor);
+    eachDiv.style.backgroundColor = eachDiv.textContent = hexaColor[index];
+    // Add copy button
+    let copy = document.createElement("button");
+    copy.textContent = "Copy";
+    copy.classList.add("copy");
+    eachDiv.appendChild(copy); 
+    
+    // document.querySelector('.copy').addEventListener("click", ()=>{
+    // copyHexaToClipBoard(hexaColor[index]);
+    // })
+  });
+};
+console.log('hexaColor-1', hexaColor);
+
+//Default example with Five colors
+exampleFiveColors();
+addColorsHexaCodeToDiv();
+
+let copyButtons = document.querySelectorAll(".copy");
+copyButtons.forEach((eachButton, index)=>{
+    //console.log("copy-Button-index", eachButton);
+    eachButton.addEventListener("click", ()=>{
+        //console.log("from click", hexaColor[index], index);
+        copyHexaToClipBoard(hexaColor[index]);
+    })
+})
+
+/*
+const addCopybuttonToDiv = ()=>{
+    let copy = document.createElement("copy");
+    copy.textContent = "Copy";
+    copy.classList.add("copy");
+    div.appendChild(copy);
+}
+*/
+const generatorButton = document.querySelector(".generatorBtn");
+
+let colorGeneratorID;
+
+const intervalColors = () => {
+  createColorDivs(); 
+  colorGeneratorID = setInterval(addColorsHexaCodeToDiv, 2000);
+  console.log("here", colorGeneratorID);
+};
+
+generatorButton.addEventListener("click", intervalColors);
+
+const stopButton = document.querySelector(".stopButton");
+
+function clearIntervalcolor() {
+  clearInterval(colorGeneratorID);
+  console.log("stop");
+}
+
+stopButton.addEventListener("click", clearIntervalcolor);
+
+/*
+
+function clearIntervalcolor(){
+    clearInterval(colorGenerator);
+}
+stopButton.addEventListener('click', clearIntervalcolor);
+/*
+
+/*
 const multiHexaGenerator = () => {
     let size = document.querySelector('.colorInput').value;
+    console.log("size test",size);
     let multiHexaStore = [];
-    for(let i=0; i< size; i++){
+    for(let i=0; i < size; i++){
         multiHexaStore.push(hexaGenerator());
     }
+    
+    //document.querySelector('.colorInput').value = "";
     return multiHexaStore;
 }
 
@@ -76,28 +157,39 @@ const displayHexaColor = () => {
     colorBoard.innerHTML = ""; // clear the previous color
     multiHexadecimal.forEach(function(eachHexa){
         let div = document.createElement('div');
+        div.classList.add('colorDiv','hexaColor');
         div.textContent = eachHexa;
         div.style.height = "5rem";
         div.style.lineHeight = "5rem";
         div.style.paddingLeft = "3rem";
         div.style.backgroundColor = eachHexa;
-        colorBoard.style.borderRadius = "10%";1
+        colorBoard.style.borderRadius = "10%";
         colorBoard.appendChild(div);
     })
     return multiHexadecimal;
 }
 
 
+//Buttons & Click Events
+const generatorButton = document.querySelector('.generatorBtn');
+const stopButton = document.querySelector('.stopButton');
 
-let colorInterval = setInterval(displayHexaColor, 200);
 
-generatorButton.addEventListener('click', ()=>{
-    displayHexaColor();
-})
+let colorGenerator;
 
-stopButton.addEventListener('click', ()=>{
-    clearInterval(colorInterval)
-})
+function intervalColors(){
+    colorGenerator = setInterval(displayHexaColor, 2000);
+}
+
+
+function clearIntervalcolor(){
+    clearInterval(colorGenerator);
+}
+
+generatorButton.addEventListener('click', intervalColors);
+stopButton.addEventListener('click', clearIntervalcolor);
 
 //displayHexaColor();
 //colorInterval()
+
+*/
